e49ccfb5c8bd6d80b8174bf57671562a125b6b47
BenoitLinux <benoit.dervieux@gmail.com>
2024-07-08 08:09:44 +0200

    principle of replacing a string functions, need to implement

--- tutorial/tuto.c
+++ tutorial/tuto.c
@@ -25,31 +25,51 @@ int main() {
     buffer[size] = '\0';
 
     // Take the word
-    const char* word = "Hello";
+    const char* word = "Hellololo";
     // Get the length of the word
     size_t length = strlen(word);
 
     printf("%s\n", buffer);
     printf("%s\n", word);
     printf("Length word: %ld\n", length);
+
     // [TODO] --> Make the insertion operation here (So take out a word and replace it for instance)
     // Maybe in the future we'll need a XMLNode to access directly the start and the end
-
-    // Initialize a new variable for the total length
-    int total = size + length + 1;
-    printf("Length total : %d\n", total);
-    // Initialize a new buffer where we will add the word
-    char * buffer2 = (char*) malloc(sizeof(char) * total);
-
-    for (int i = 0; i < size; i++) {
-        buffer2[i] = buffer[i];
+    int start = 5;
+    int end = 10;
+    for (int i = start; i < end; i++) {
+        printf("%c", buffer[i]);
     }
-    int index= 0;
-    for (int i = size; i < total; i++) {
-        buffer2[i] = word[index++];
+    printf("\n");
+
+    // Here what we will do is to take the size of the buffer, substract the (end - start)
+    // and then add the length of the word. --> this will give the new size of the buffer
+    // [PROBLEM] --> Now we need to think about the insertion but I believe this is
+    // where the start "starts"
+
+    int final_size = size - (end - start) + length + 1;
+    char * buffer_fsize = (char*) malloc(sizeof(char) * final_size);
+
+    // Here the loop will be more complicated
+    // Implement the first loop up to the index 
+    for (int i = 0; i < start; i++) {
+        buffer_fsize[i] = buffer[i];
     }
-    buffer2[total] = '\0';
-    printf("%s\n", buffer2);
+    // Implement the second loop for the replacing word
+    // from the start index to the length of the new word
+    for (int i = start; i < start + length; i++) {
+        buffer_fsize[i] = word[i-start];
+    }
+    // implement the rest of the document
+    // from the end index to the end of the buffer 1
+    for(int i = end; i < size; i++) {
+        buffer_fsize[start + length + (i-end)] = buffer[i];
+    }
+
+    buffer_fsize[final_size] = '\0';
+    printf("%s\n", buffer_fsize);
+    printf("*************************\n");
+
     
     fclose(file);
 
@@ -58,8 +78,8 @@ int main() {
         printf("Error opening file\n");
         return 1;
     }
-    for (int i = 0; i < total - 1; i++) {
-        fputc(buffer2[i], file_to_write_to);
+    for (int i = 0; i < final_size - 1; i++) {
+        fputc(buffer_fsize[i], file_to_write_to);
     }
     fclose(file_to_write_to);
     return 0;
